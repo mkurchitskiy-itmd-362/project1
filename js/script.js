b6e111e11a6c86ccdf59d62eece22f485a73e0cb
Mad <maxkurchitskiy@gmail.com>
2023-10-12 21:59:03 -0500

    Added jQuery feature to submit bottom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
-    $('html').removeClass('nojs');
-    $('html').addClass('hasjs');
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+$(document).ready(function () {
+  $("#contact-form").click(function () {
+    alert("Thank you for subcribing, time to ride or died!");
   });
+}); 
